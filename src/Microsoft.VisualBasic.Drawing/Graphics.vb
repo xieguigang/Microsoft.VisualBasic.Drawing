1c0ce0cfe18de38a688ea068783e2c3c677a5a7d
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-17 21:57:03 +0800

    implements of the canvas based on skiasharp

--- src/Microsoft.VisualBasic.Drawing/Graphics.vb
+++ src/Microsoft.VisualBasic.Drawing/Graphics.vb
@@ -1,10 +1,18 @@
 ﻿Imports System.Drawing
-Imports Microsoft.VisualBasic.ComponentModel.Collection
+Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports Microsoft.VisualBasic.Imaging
+Imports SkiaSharp
 
+''' <summary>
+''' A wrapper of the skia sharp canvas object
+''' </summary>
 Public Class Graphics
 
-    ReadOnly m_pixels As Color()()
+    ReadOnly m_info As SKImageInfo
     ReadOnly m_size As Size
+    ReadOnly m_surface As SKSurface
+    ReadOnly m_canvas As SKCanvas
 
     Public ReadOnly Property Width As Integer
         Get
@@ -20,7 +28,9 @@ Public Class Graphics
 
     Sub New(size As Size, Optional fill As Color? = Nothing)
         m_size = size
-        m_pixels = RectangularArray.Matrix(Of Color)(size.Width, size.Height)
+        m_info = New SKImageInfo(size.Width, size.Height)
+        m_surface = SKSurface.Create(m_info)
+        m_canvas = m_surface.Canvas
 
         If fill Is Nothing Then
             Call Clear(Color.Transparent)
@@ -30,27 +40,46 @@ Public Class Graphics
     End Sub
 
     Public Function GetPixel(x As Integer, y As Integer) As Color
-        Return m_pixels(y)(x)
+        Throw New NotImplementedException
     End Function
 
     Public Sub SetPixel(x As Integer, y As Integer, pixel As Color)
-        m_pixels(y)(x) = pixel
+        Throw New NotImplementedException
     End Sub
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Sub Clear(fill As Color)
-        For Each row As Color() In m_pixels
-            For i As Integer = 0 To row.Length - 1
-                row(i) = fill
-            Next
-        Next
+        Call m_canvas.Clear(fill.AsSKColor)
     End Sub
 
     Public Sub DrawString(s As String, font As Font, color As Color, x As Single, y As Single)
+        Dim textPain As New SKPaint With {
+           .IsAntialias = True,
+           .Style = SKPaintStyle.Fill,
+           .Color = color.AsSKColor,
+           .TextSize = font.Size
+        }
 
+        m_canvas.DrawText(s, x, y, textPain)
     End Sub
 
     Public Overrides Function ToString() As String
         Return "raster_buffer: " & StringFormats.Lanudry(Width * Height * 4&)
+    End Function
+
+    Public Function Save(file As Stream, format As ImageFormats) As Boolean
+        Dim image = m_surface.Snapshot
+        Dim data = image.Encode(SKEncodedImageFormat.Png, 100)
+
+        Try
+            Call data.SaveTo(file)
+            Call file.Flush()
+        Catch ex As Exception
+            Call App.LogException(ex)
+            Return False
+        End Try
+
+        Return True
     End Function
 
 End Class
---
+++ src/Microsoft.VisualBasic.Drawing/Interop.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Drawing
+Imports System.Runtime.CompilerServices
+Imports SkiaSharp
+
+Module Interop
+
+    <Extension>
+    Public Function AsSKColor(color As Color) As SKColor
+        Throw New NotImplementedException
+    End Function
+
+End Module
