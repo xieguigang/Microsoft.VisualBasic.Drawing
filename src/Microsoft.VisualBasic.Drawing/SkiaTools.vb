882a66c9aedf9efd8970cea9db3de42dd25f210c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-20 14:47:13 +0800

    make image conversion

---
+++ src/Microsoft.VisualBasic.Drawing/SkiaTools.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Runtime.CompilerServices
+Imports SkiaSharp
+
+Public Module SkiaTools
+
+    <Extension>
+    Public Function ConvertToPngFile(svg As SkiaSharp.Extended.Svg.SKSvg) As SKImage
+        Using bitmap As New SKBitmap(svg.Picture.CullRect.Width, svg.Picture.CullRect.Height)
+            Using canvas As New SKCanvas(bitmap)
+                Call canvas.DrawPicture(svg.Picture)
+                Return SKImage.FromBitmap(bitmap)
+            End Using
+        End Using
+    End Function
+
+    <Extension>
+    Public Sub ConvertToPdfFile(svg As SkiaSharp.Extended.Svg.SKSvg, pdffile As String)
+        Using pdfDocument = SKDocument.CreatePdf(pdffile)
+            Dim canvas = pdfDocument.BeginPage(svg.Picture.CullRect.Width, svg.Picture.CullRect.Height)
+
+            Call canvas.DrawPicture(svg.Picture)
+            Call pdfDocument.EndPage()
+            Call pdfDocument.Close()
+        End Using
+    End Sub
+End Module
