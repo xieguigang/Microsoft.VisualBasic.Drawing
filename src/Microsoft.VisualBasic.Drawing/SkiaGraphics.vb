293f8a0b4fd9f7dc326a13370cb73005f28c3faa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-09-24 19:55:05 +0800

    implements interface

--- src/Microsoft.VisualBasic.Drawing/SkiaGraphics.vb
+++ src/Microsoft.VisualBasic.Drawing/SkiaGraphics.vb
@@ -9,23 +9,16 @@ Public MustInherit Class SkiaGraphics : Inherits IGraphics
     Protected ReadOnly canvasRect As SKRect
     Protected m_canvas As SKCanvas
 
-    Public ReadOnly Property Width As Integer
-        Get
-            Return Size.Width
-        End Get
-    End Property
-
-    Public ReadOnly Property Height As Integer
-        Get
-            Return Size.Height
-        End Get
-    End Property
-
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Sub New(width As Integer, height As Integer)
         Call MyBase.New(width, height)
         canvasRect = SKRect.Create(width, height)
     End Sub
+
+    Public Overrides ReadOnly Property Size As Size
+    Public Overrides Property PageScale As Single
+    Public Overrides Property RenderingOrigin As Point
+    Public Overrides Property TextContrast As Integer
 
     <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Overrides Sub Clear(fill As Color)
@@ -48,6 +41,22 @@ Public MustInherit Class SkiaGraphics : Inherits IGraphics
         m_canvas.DrawText(s, x, y, textPain)
     End Sub
 
+    Public Overrides Sub DrawString(s As String, font As Font, brush As Brush, ByRef point As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawString(s As String, font As Font, brush As Brush, layoutRectangle As RectangleF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawString(s As String, font As Font, brush As Brush, ByRef x As Single, ByRef y As Single, angle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawString(s As String, font As Font, brush As Brush, x As Single, y As Single)
+        Throw New NotImplementedException()
+    End Sub
+
     Public Overrides Sub DrawLine(x1 As Single, y1 As Single, x2 As Single, y2 As Single, color As Color, width As Single,
                                   Optional dash As Single() = Nothing)
 
@@ -61,6 +70,22 @@ Public MustInherit Class SkiaGraphics : Inherits IGraphics
 
             Call m_canvas.DrawLine(x1, y1, x2, y2, paint)
         End Using
+    End Sub
+
+    Public Overrides Sub DrawLine(pen As Pen, pt1 As PointF, pt2 As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawLine(pen As Pen, pt1 As Point, pt2 As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawLine(pen As Pen, x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawLine(pen As Pen, x1 As Single, y1 As Single, x2 As Single, y2 As Single)
+        Throw New NotImplementedException()
     End Sub
 
     Public Overrides Sub DrawPath(path As Polygon2D, color As Color, width As Single,
@@ -102,6 +127,346 @@ Public MustInherit Class SkiaGraphics : Inherits IGraphics
         End Using
     End Sub
 
+    Public Overrides Sub DrawPath(pen As Pen, path As GraphicsPath)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub AddMetafileComment(data() As Byte)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub Dispose()
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawArc(pen As Pen, rect As RectangleF, startAngle As Single, sweepAngle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawArc(pen As Pen, rect As Rectangle, startAngle As Single, sweepAngle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawArc(pen As Pen, x As Integer, y As Integer, width As Integer, height As Integer, startAngle As Integer, sweepAngle As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawArc(pen As Pen, x As Single, y As Single, width As Single, height As Single, startAngle As Single, sweepAngle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawBezier(pen As Pen, pt1 As Point, pt2 As Point, pt3 As Point, pt4 As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawBezier(pen As Pen, pt1 As PointF, pt2 As PointF, pt3 As PointF, pt4 As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawBezier(pen As Pen, x1 As Single, y1 As Single, x2 As Single, y2 As Single, x3 As Single, y3 As Single, x4 As Single, y4 As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawBeziers(pen As Pen, points() As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawBeziers(pen As Pen, points() As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawClosedCurve(pen As Pen, points() As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawClosedCurve(pen As Pen, points() As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawCurve(pen As Pen, points() As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawCurve(pen As Pen, points() As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawCurve(pen As Pen, points() As PointF, tension As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawCurve(pen As Pen, points() As Point, tension As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawCurve(pen As Pen, points() As PointF, offset As Integer, numberOfSegments As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawCurve(pen As Pen, points() As PointF, offset As Integer, numberOfSegments As Integer, tension As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawCurve(pen As Pen, points() As Point, offset As Integer, numberOfSegments As Integer, tension As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawEllipse(pen As Pen, rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawEllipse(pen As Pen, rect As RectangleF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawEllipse(pen As Pen, x As Single, y As Single, width As Single, height As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawEllipse(pen As Pen, x As Integer, y As Integer, width As Integer, height As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, point As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, destPoints() As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, destPoints() As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, point As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, rect As RectangleF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, x As Integer, y As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, x As Single, y As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, x As Single, y As Single, width As Single, height As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImage(image As Imaging.Image, x As Integer, y As Integer, width As Integer, height As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImageUnscaled(image As Imaging.Image, rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImageUnscaled(image As Imaging.Image, point As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImageUnscaled(image As Imaging.Image, x As Integer, y As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImageUnscaled(image As Imaging.Image, x As Integer, y As Integer, width As Integer, height As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawImageUnscaledAndClipped(image As Imaging.Image, rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawLines(pen As Pen, points() As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawLines(pen As Pen, points() As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawPie(pen As Pen, rect As Rectangle, startAngle As Single, sweepAngle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawPie(pen As Pen, rect As RectangleF, startAngle As Single, sweepAngle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawPie(pen As Pen, x As Integer, y As Integer, width As Integer, height As Integer, startAngle As Integer, sweepAngle As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawPie(pen As Pen, x As Single, y As Single, width As Single, height As Single, startAngle As Single, sweepAngle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawCircle(center As PointF, fill As Color, stroke As Pen, radius As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawPolygon(pen As Pen, points() As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawPolygon(pen As Pen, points() As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawRectangle(pen As Pen, rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawRectangle(pen As Pen, rect As RectangleF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawRectangle(pen As Pen, x As Single, y As Single, width As Single, height As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawRectangle(pen As Pen, x As Integer, y As Integer, width As Integer, height As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawRectangles(pen As Pen, rects() As RectangleF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub DrawRectangles(pen As Pen, rects() As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub ExcludeClip(rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillClosedCurve(brush As Brush, points() As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillClosedCurve(brush As Brush, points() As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillEllipse(brush As Brush, rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillEllipse(brush As Brush, rect As RectangleF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillEllipse(brush As Brush, x As Single, y As Single, width As Single, height As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillEllipse(brush As Brush, x As Integer, y As Integer, width As Integer, height As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillPath(brush As Brush, path As GraphicsPath)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillPie(brush As Brush, rect As Rectangle, startAngle As Single, sweepAngle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillPie(brush As Brush, x As Integer, y As Integer, width As Integer, height As Integer, startAngle As Integer, sweepAngle As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillPie(brush As Brush, x As Single, y As Single, width As Single, height As Single, startAngle As Single, sweepAngle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillPolygon(brush As Brush, points() As Point)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillPolygon(brush As Brush, points() As PointF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillRectangle(brush As Brush, rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillRectangle(brush As Brush, rect As RectangleF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillRectangle(brush As Brush, x As Integer, y As Integer, width As Integer, height As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub FillRectangle(brush As Brush, x As Single, y As Single, width As Single, height As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub Flush()
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub IntersectClip(rect As RectangleF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub IntersectClip(rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub ResetClip()
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub ResetTransform()
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub RotateTransform(angle As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub ScaleTransform(sx As Single, sy As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub SetClip(rect As RectangleF)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub SetClip(rect As Rectangle)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub TranslateClip(dx As Single, dy As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub TranslateClip(dx As Integer, dy As Integer)
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Overrides Sub TranslateTransform(dx As Single, dy As Single)
+        Throw New NotImplementedException()
+    End Sub
+
+    Protected Overrides Sub ClearCanvas(color As Color)
+        Throw New NotImplementedException()
+    End Sub
+
     Public Overrides Function MeasureString(text As String, fontName As String, fontSize As Single) As (Width As Single, Height As Single)
         Using paint As New SKPaint With {
             .TextSize = fontSize,
@@ -113,5 +478,41 @@ Public MustInherit Class SkiaGraphics : Inherits IGraphics
             Call paint.MeasureText(text, textBounds)
             Return (textBounds.Width, textBounds.Height)
         End Using
+    End Function
+
+    Public Overrides Function MeasureString(text As String, font As Font) As SizeF
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function MeasureString(text As String, font As Font, width As Integer) As SizeF
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function MeasureString(text As String, font As Font, layoutArea As SizeF) As SizeF
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function GetContextInfo() As Object
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function GetNearestColor(color As Color) As Color
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function IsVisible(rect As Rectangle) As Boolean
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function IsVisible(rect As RectangleF) As Boolean
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function IsVisible(x As Integer, y As Integer, width As Integer, height As Integer) As Boolean
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function IsVisible(x As Single, y As Single, width As Single, height As Single) As Boolean
+        Throw New NotImplementedException()
     End Function
 End Class
