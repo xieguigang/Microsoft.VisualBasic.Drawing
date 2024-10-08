d8b73b70b3735d5cde525bd7e205dccbfc453787
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-08 22:30:54 +0800

    pdf image wrapper

---
+++ src/Microsoft.VisualBasic.Drawing/Interop/PdfImage.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Drawing
+Imports System.IO
+Imports Microsoft.VisualBasic.Imaging.Driver
+Imports Microsoft.VisualBasic.MIME.Html.CSS
+Imports Microsoft.VisualBasic.Net.Http
+
+Public Class PdfImage : Inherits GraphicsData
+
+    Public Overrides ReadOnly Property Driver As Drivers
+        Get
+            Return Drivers.PDF
+        End Get
+    End Property
+
+    Public Sub New(img As Object, size As Size, padding As Padding)
+        MyBase.New(img, size, padding)
+    End Sub
+
+    Public Overrides Function GetDataURI() As DataURI
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function Save(path As String) As Boolean
+        Throw New NotImplementedException()
+    End Function
+
+    Public Overrides Function Save(out As Stream) As Boolean
+        Throw New NotImplementedException()
+    End Function
+End Class
