b9cecec917021d0b343573b7ca450c27de55d2ad
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-14 15:16:14 +0800

    add demo plot image outputs

--- src/Microsoft.VisualBasic.Drawing/test/test23.vb
+++ src/Microsoft.VisualBasic.Drawing/test/test23.vb
@@ -9,12 +9,27 @@ Module test23
         Call Rendering(New Graphics(256, 256, "#ffffff")).Save("./RedBlueArgb32GradientWithAlpha.png")
         Call Rendering(New SvgGraphics(256, 256)).Save("./RedBlueArgb32GradientWithAlpha.svg")
         Call Rendering(New PdfGraphics(256, 256)).Save("./RedBlueArgb32GradientWithAlpha.pdf")
+
+        Call Rendering2(New Graphics(256, 256, "#ffffff")).Save("./RedGreen24BitGradient.bmp", ImageFormats.Bmp)
+        Call Rendering2(New Graphics(256, 256, "#ffffff")).Save("./RedGreen24BitGradient.png")
+        Call Rendering2(New SvgGraphics(256, 256)).Save("./RedGreen24BitGradient.svg")
+        Call Rendering2(New PdfGraphics(256, 256)).Save("./RedGreen24BitGradient.pdf")
     End Sub
+
+    Private Function Rendering2(Of T As SkiaGraphics)(g As T) As T
+        For row As Integer = 0 To g.Height - 1
+            For column As Integer = 0 To g.Width - 1
+                Call g.FillRectangle(New SolidBrush(Color.FromArgb(255, row, column, 0)), New Rectangle(row, column, 1, 1))
+            Next
+        Next
+
+        Return g
+    End Function
 
     Private Function Rendering(Of T As SkiaGraphics)(g As T) As T
         For row As Integer = 0 To g.Height - 1
             For column As Integer = 0 To g.Width - 1
-                Call g.FillRectangle(New SolidBrush(Color.FromArgb((row + column) / 2, row, 0, column)), New Rectangle(row, column, 1, 1))
+                Call g.FillRectangle(New SolidBrush(Color.FromArgb(255, row, 0, column)), New Rectangle(row, column, 1, 1))
             Next
         Next
 
