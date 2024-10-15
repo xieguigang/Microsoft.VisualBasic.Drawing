8a8f26e41fee4721a279e58cc702d868d927d869
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-15 09:54:50 +0800

    populate the graphics path builder operations

--- src/Microsoft.VisualBasic.Drawing/Interop/PathBuilder.vb
+++ src/Microsoft.VisualBasic.Drawing/Interop/PathBuilder.vb
@@ -6,6 +6,10 @@ Module PathBuilder
 
     <Extension>
     Public Function CreatePath(path As GraphicsPath) As SKPath
+        Dim skia As New SKPath
 
+
+
+        Return skia
     End Function
 End Module
