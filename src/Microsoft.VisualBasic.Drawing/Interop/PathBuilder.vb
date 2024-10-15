34b274003f537977d9980f775e42a11fb8fba404
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-10-15 10:06:13 +0800

    create arc

--- src/Microsoft.VisualBasic.Drawing/Interop/PathBuilder.vb
+++ src/Microsoft.VisualBasic.Drawing/Interop/PathBuilder.vb
@@ -1,6 +1,9 @@
 ﻿Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Linq
 Imports SkiaSharp
+Imports Microsoft.VisualBasic.Math
+Imports Microsoft.VisualBasic.Math.SIMD.SIMDIntrinsics
 
 Module PathBuilder
 
@@ -8,8 +11,31 @@ Module PathBuilder
     Public Function CreatePath(path As GraphicsPath) As SKPath
         Dim skia As New SKPath
 
-
+        For Each op As GraphicsPath.op In path.AsEnumerable
+            Select Case op.GetType
+                Case GetType(GraphicsPath.op_AddArc) : Call skia.AddArc(op)
+                Case Else
+                    Throw New NotImplementedException(op.GetType.FullName)
+            End Select
+        Next
 
         Return skia
     End Function
+
+    <Extension>
+    Private Sub AddArc(skia As SKPath, arc As GraphicsPath.op_AddArc)
+        Dim startAngleRad = Trigonometric.ToRadians(arc.startAngle)
+        Dim sweepAngleRad = Trigonometric.ToRadians(arc.sweepAngle)
+        Dim rx = arc.rect.Width / 2.0F
+        Dim ry = arc.rect.Height / 2.0F
+        Dim cx = arc.rect.Left + rx
+        Dim cy = arc.rect.Top + ry
+
+        Call skia.ArcTo(
+            New SKRect(arc.rect.Left, arc.rect.Top, arc.rect.Right, arc.rect.Bottom),
+            startAngleRad,
+            sweepAngleRad,
+            False
+        )
+    End Sub
 End Module
