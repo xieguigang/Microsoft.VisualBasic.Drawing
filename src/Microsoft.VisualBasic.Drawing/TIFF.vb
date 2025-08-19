7bd74b61fc56d87d15adf0564710e6297a34156a
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-08-19 11:38:26 +0800

    save

---
+++ src/Microsoft.VisualBasic.Drawing/TIFF.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports SkiaSharp
+
+Public Module TIFF
+
+    '<Extension>
+    'Public Sub SaveTiff(layers As IEnumerable(Of SKBitmap), file As Stream)
+    '    Using encoder As New SKMultiFrameImageEncoder(file, SKEncodedImageFormat.Tiff, 100)
+
+    '    End Using
+    'End Sub
+End Module
